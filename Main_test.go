6e01500885f1db93cb9a9fd160bd99d54fde789e
Sergey Pustovalov <pustserg@yandex.ru>
2016-08-07 14:32:22 +0300

    Add benchmark

---
+++ Main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+// at 37abb4760815343adbdd2e50090131f2ddcd3e11
+//PASS
+//1	16794929234 ns/op
+//ok  	rasp_parser	16.814s
+import "testing"
+
+func BenchmarkMain(b *testing.B)  {
+	for n:=0; n < b.N; n++ {
+		main()
+	}
+}
